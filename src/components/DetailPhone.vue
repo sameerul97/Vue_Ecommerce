8219a2d622e6fcdd1f4acf6f6b94ead74f13cadd
sameerul97 <sameerul97@gmail.com>
2019-02-19 18:17:41 +0000

    Posting a question, answering a question is done. binding the ID in Vue was very simple and neat. As modal component can be registered globaly, its more easy to bring in the component

--- src/components/DetailPhone.vue
+++ src/components/DetailPhone.vue
@@ -116,6 +116,90 @@
         </table>
       </div>
       <div class="bg-light rounded text-center p-3 mb-5">
+        <p
+          class="font-weight-light text-capitalize text-center"
+          style="font-size:1.5rem;"
+        >Customer questions & answers</p>
+        <div class="row">
+          <div class="col-sm-8">
+            <div v-if="questionAndAnswers.length > 1">
+              <div
+                v-for="(qandA,index) in questionAndAnswers"
+                :key="index"
+                class="shadow-sm bg-light rounded text-left"
+              >
+                <div class="border p-3 mb-5 mt-4">
+                  <p class="font-weight-bold text-primary">{{qandA.question}}</p>
+                  <div class="card-footer text-muted">
+                    <img
+                      src="https://images-eu.ssl-images-amazon.com/images/S/amazon-avatars/default._CR0,0,1024,1024_SX48_.png"
+                    >
+                    {{qandA.userName}}
+                    <span class="float-right mt-1">
+                      <a
+                        class="btn btn-info text-light btn-sm"
+                        data-toggle="modal"
+                        v-on:click="loadQuestionIntoModal(qandA)"
+                        data-target="#writingAnAnswer"
+                      >Answer {{qandA.userName}} question</a>
+                    </span>
+                  </div>
+                  <div v-if="qandA.answers.length <= 2" >
+                  <div v-for="(qandA,index) in qandA.answers" :key="index" class="p-1 ml-3 mb-2 mt-2">
+                    <p class="font-weight-light">{{qandA.answer}}</p>
+                    <div class="card-footer text-muted">
+                      <img src="https://images-eu.ssl-images-amazon.com/images/S/amazon-avatars/default._CR0,0,1024,1024_SX48_.png">
+                      Answered by {{qandA.userName}}
+                    </div>
+                  </div>
+                  </div>
+                  <div v-else>
+                  <div class="accordion" id="accordionExample">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link " type="button" data-toggle="collapse" v-bind:data-target="'#demo'+ qandA._id"
+                        aria-expanded="false" aria-controls="collapseOne">
+                        <i class="fas fa-chevron-circle-down"></i>
+                      </button>
+                      <span class="btn btn-link fullSpecLink" data-toggle="collapse" v-bind:data-target="'#demo'+ qandA._id"
+                        aria-expanded="false" aria-controls="collapseOne">View answer</span>
+                    </h5>
+                    <div :id="'demo'+ qandA._id" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                      <div v-for="(qandA,index) in qandA.answers" :key="index" class="p-1 ml-3 mb-2 mt-2">
+                        <p class="font-weight-light">{{qandA.answer}}</p>
+                        <div class="card-footer text-muted">
+                          <img src="https://images-eu.ssl-images-amazon.com/images/S/amazon-avatars/default._CR0,0,1024,1024_SX48_.png">
+                          Answered by {{qandA.userName}}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-else>
+              No Question and answers! Be the first to Question
+              </div>
+          </div>
+
+          <div class="col-sm-4">
+            <hr style="margin-top: 22px!important;margin-bottom: 22px!important;">
+            <div class="text-left">
+              <p class="font-weight-bold">Answer a question</p>
+              <button
+                type="button"
+                class="btn btn-outline-info btn-block"
+                data-toggle="modal"
+                data-target="#postQuestion"
+              >
+                Post
+                question
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-light rounded text-center p-3 mb-5">
         <p class="font-weight-light text-capitalize text-center" style="font-size:1.5rem;">Reviews</p>
         <div class="row">
           <div class="col-sm-4">
@@ -306,12 +390,83 @@
               </div>
             </div>
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-secondary" id="closeButton" data-dismiss="modal">Close</button>
               <button type="button" class="btn btn-success" v-on:click="sendReview()">Post Review</button>
             </div>
           </div>
         </div>
       </div>
+        <div class="modal fade bd-example-modal-lg" id="writingAnAnswer" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Write answer</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="container text-left">
+          <div v-if="successState">
+                  <div class="alert alert-success" role="alert">{{loggInMessage}}</div>
+                </div>
+          <div>
+            {{question}}
+          </div>
+          <hr class="customTopLine customBottonLine">
+          <span>Enter your answer</span>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">With textarea</span>
+            </div>
+            <textarea class="form-control" v-model="myAnswer" aria-label="With textarea"></textarea>
+          </div>
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" id="AnswerModalcloseButton" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success" v-on:click="sendAnswer()">Post Review</button>
+      </div>
+      
+    </div>
+  </div>
+</div>
+<!-- Post quesion modal -->
+<div class="modal fade bd-example-modal-lg" id="postQuestion" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"
+  aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalCenterTitle">Ask your question</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="container text-left">
+          <div v-if="successState">
+                  <div class="alert alert-success" role="alert">{{loggInMessage}}</div>
+                </div>
+          <hr class="customTopLine customBottonLine">
+          <span>Enter your question</span>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">Question</span>
+            </div>
+            <textarea class="form-control" v-model="myQuestion" aria-label="With textarea"></textarea>
+          </div>
+        </div>
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" id="loadQuestionIntoModalCloseButton" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success" v-on:click="sendQuestion()">Post Review</button>
+      </div>
+    </div>
+  </div>
+</div>
     </div>
   </div>
 </template>
@@ -338,11 +493,39 @@ export default {
       userReviews: [],
       reviewSuccess: "",
       userGivenStar: "",
-      userGivenOpinion: ""
+      userGivenOpinion: "",
+      questionAndAnswers: [], 
+      question : "",
+      selectedQuestion : "", 
+      myAnswer: "",
+      myQuestion: "",
     };
   },
   mounted: function() {
-    var mobileIdToSearch = this.$route.params.mobileId;
+    this.reloadQuestionAndAnswerComp();
+  },
+  methods: {
+    checkTokenValidation: function() {
+      fetch("http://localhost:3000/checkToken", {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Bearer " + localStorage.getItem("token")
+        }
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(myJson => {
+          this.tokenValidationBool = myJson.Message;
+          return this.tokenValidationBool;
+        });
+      return this.tokenValidationBool;
+    },
+    reloadQuestionAndAnswerComp: function(){
+      this.questionAndAnswer = [];
+          var mobileIdToSearch = this.$route.params.mobileId;
     fetch("http://localhost:3000/getPhone/" + mobileIdToSearch)
       .then(function(response) {
         return response.json();
@@ -404,25 +587,128 @@ export default {
           this.userReviews = [];
         }
       });
-  },
-  methods: {
-    checkTokenValidation: function() {
-      fetch("http://localhost:3000/checkToken", {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
+    fetch("http://localhost:3000/getQAndAs/" + mobileIdToSearch)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(myJson);
+        return myJson.qAndAs;
+      })
+      .then(details => {
+        this.questionAndAnswers = details;
+      });
+    },
+    sendQuestion:function(){
+      console.log(this.myQuestion) // Samsung S8 or Iphone camera ? 
+            fetch("http://localhost:3000/postQuestion/", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
         mode: "cors", // no-cors, cors, *same-origin
         headers: {
           "Content-Type": "application/x-www-form-urlencoded",
           Authorization: "Bearer " + localStorage.getItem("token")
-        }
+        },
+        body: new URLSearchParams(
+          "userId=" +
+          localStorage.getItem("userId") +
+          "&userName=" +
+          localStorage.getItem("name") +
+          "&question=" +
+          this.myQuestion +
+          "&mobileId=" +
+          this.phone.mobileId
+        ) // body data type must match "Content-Type" header
       })
         .then(function(response) {
           return response.json();
         })
         .then(myJson => {
-          this.tokenValidationBool = myJson.Message;
-          return this.tokenValidationBool;
+          console.log(myJson);
+          if (myJson.Message != "Success") {
+            this.successState = true;
+            this.loggInMessage =
+              myJson.Message + "! Please login before continuing";
+            setTimeout(
+              function() {
+                this.loggInMessage = "";
+                this.successState = false;
+                 var element = document.getElementById("loadQuestionIntoModalCloseButton");
+                 element.click();
+              }.bind(this),
+              5000
+            );
+          } else {
+            this.successState = true;
+            this.loggInMessage = "Question posted";
+            setTimeout(
+              function() {
+                this.loggInMessage = "";
+                this.successState = false;
+                 var element = document.getElementById("loadQuestionIntoModalCloseButton") ;
+                  element.click();
+                this.reloadQuestionAndAnswerComp();
+              }.bind(this),
+              3000
+            );
+          }
         });
-      return this.tokenValidationBool;
+    },
+    loadQuestionIntoModal: function(qandA) {
+      console.log(qandA);
+      this.question= qandA.question;
+      this.selectedQuestion=qandA;
+    },
+    sendAnswer:function(){
+      console.log(this.myAnswer);
+      fetch("http://localhost:3000/postAnswer/", {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Bearer " + localStorage.getItem("token")
+        },
+        body: new URLSearchParams(
+          "userId=" +
+          localStorage.getItem("userId") +
+          "&userName=" +
+          localStorage.getItem("name") +
+          "&answer=" +
+          this.myAnswer +
+          "&questionId=" +
+          this.selectedQuestion._id
+        ) // body data type must match "Content-Type" header
+      })
+        .then(function(response) {
+          return response.json();
+        })
+        .then(myJson => {
+          console.log(myJson);
+          if (myJson.Message != "Success") {
+            this.successState = true;
+            this.loggInMessage =
+              myJson.Message + "! Please login before continuing";
+            setTimeout(
+              function() {
+                this.loggInMessage = "";
+                this.successState = false;
+              }.bind(this),
+              5000
+            );
+          } else {
+            this.successState = true;
+            this.loggInMessage = "Answer posted";
+            setTimeout(
+              function() {
+                this.loggInMessage = "";
+                this.successState = false;
+                var element = document.getElementById("AnswerModalcloseButton");
+                element.click();
+                this.reloadQuestionAndAnswerComp();
+              }.bind(this),
+              3000
+            );
+          }
+        });
     },
     addToBasket: function(phone) {
       const token = localStorage.getItem("token");
@@ -529,6 +815,9 @@ export default {
               function() {
                 this.loggInMessage = "";
                 this.successState = false;
+                var element = document.getElementById("closeButton");
+        element.click();
+        this.reloadQuestionAndAnswerComp();
               }.bind(this),
               3000
             );
@@ -584,49 +873,49 @@ export default {
 <style>
 @import url(//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css);
 
-.imagePlaceHolder > img{
-    height: 400px !important;
+.imagePlaceHolder > img {
+  height: 400px !important;
 }
 .btn-primary {
-    font-family: Raleway-SemiBold;
-    font-size: 18px;
-    color: rgba(58, 133, 191, 0.75);
-    letter-spacing: 1px;
-    line-height: 15px;
-    border: 2px solid rgba(58, 133, 191, 0.75);
-    border-radius: 40px;
-    background: transparent;
-    transition: all 0.3s ease 0s;
-  }
-  
-  .btn-primary:hover {
-    color: #FFF;
-    background: rgba(58, 133, 191, 0.75);
-    border: 2px solid rgba(58, 133, 191, 0.75);
-  }
+  font-family: Raleway-SemiBold;
+  font-size: 18px;
+  color: rgba(58, 133, 191, 0.75);
+  letter-spacing: 1px;
+  line-height: 15px;
+  border: 2px solid rgba(58, 133, 191, 0.75);
+  border-radius: 40px;
+  background: transparent;
+  transition: all 0.3s ease 0s;
+}
 
-  i{
-    color: rgba(58, 133, 191, 0.75);
-  }
-    .noStar{
-    color: tan;
-  }
-  .checked {
-    color: orange;
-  }
-  .totalReviews{
-    font-weight: 300;
-  }
-  
+.btn-primary:hover {
+  color: #fff;
+  background: rgba(58, 133, 191, 0.75);
+  border: 2px solid rgba(58, 133, 191, 0.75);
+}
+
+i {
+  color: rgba(58, 133, 191, 0.75);
+}
+.noStar {
+  color: tan;
+}
+.checked {
+  color: orange;
+}
+.totalReviews {
+  font-weight: 300;
+}
+
 /* Three column layout */
 .side {
   float: left;
   width: 15%;
-  margin-top:10px;
+  margin-top: 10px;
 }
 
 .middle {
-  margin-top:19px;
+  margin-top: 19px;
   float: left;
   width: 70%;
 }
@@ -645,41 +934,72 @@ export default {
 }
 
 /* Individual bars */
-.bar-5 {width: 60%; height: 18px; background-color: #4CAF50;}
-.bar-4 {width: 30%; height: 18px; background-color: #2196F3;}
-.bar-3 {width: 10%; height: 18px; background-color: #00bcd4;}
-.bar-2 {width: 4%; height: 18px; background-color: #ff9800;}
-.bar-1 {width: 15%; height: 18px; background-color: #f44336;}
+.bar-5 {
+  width: 60%;
+  height: 18px;
+  background-color: #4caf50;
+}
+.bar-4 {
+  width: 30%;
+  height: 18px;
+  background-color: #2196f3;
+}
+.bar-3 {
+  width: 10%;
+  height: 18px;
+  background-color: #00bcd4;
+}
+.bar-2 {
+  width: 4%;
+  height: 18px;
+  background-color: #ff9800;
+}
+.bar-1 {
+  width: 15%;
+  height: 18px;
+  background-color: #f44336;
+}
 
 /* Responsive layout - make the columns stack on top of each other instead of next to each other */
 @media (max-width: 400px) {
-  .side, .middle {
+  .side,
+  .middle {
     width: 100%;
   }
   .right {
     display: none;
   }
 }
-.userIcon{
-  height:48px !important; 
+.userIcon {
+  height: 48px !important;
 }
 
-.customLine{
-  margin-top: 22px!important;margin-bottom: 22px!important;
+.customLine {
+  margin-top: 22px !important;
+  margin-bottom: 22px !important;
 }
 
-fieldset, label { margin: 0; padding: 0; }
-h1 { font-size: 1.5em; margin: 10px; }
+fieldset,
+label {
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  font-size: 1.5em;
+  margin: 10px;
+}
 
 /****** Style Star Rating Widget *****/
 
-.rating { 
+.rating {
   border: none;
   float: left;
 }
 
-.rating > input { display: none; } 
-.rating > label:before { 
+.rating > input {
+  display: none;
+}
+.rating > label:before {
   margin: 5px;
   font-size: 1.25em;
   font-family: FontAwesome;
@@ -687,27 +1007,27 @@ h1 { font-size: 1.5em; margin: 10px; }
   content: "\f005";
 }
 
-.rating > .half:before { 
+.rating > .half:before {
   content: "\f089";
   position: absolute;
 }
 
-.rating > label { 
-  color: #ddd; 
- float: right; 
+.rating > label {
+  color: #ddd;
+  float: right;
 }
-.customTopLine{
-  margin-top: 50px!important;
+.customTopLine {
+  margin-top: 50px !important;
 }
-.customBottomLine{
-  margin-top: 26px!important;
+.customBottomLine {
+  margin-top: 26px !important;
 }
 
 /***** CSS Magic to Highlight Stars on Hover *****/
 
 .rating > input:checked ~ label, /* show gold star when clicked */
 .rating:not(:checked) > label:hover, /* hover current star */
-.rating:not(:checked) > label:hover ~ label { color: #FFD700;  } /* hover previous stars in list */
-
-
+.rating:not(:checked) > label:hover ~ label {
+  color: #ffd700;
+} /* hover previous stars in list */
 </style>
